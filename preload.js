ab803d1ec05a93125a25db77f1ef67ea86a000c2
Polofiziert <polo@polofiziert.de>
2026-02-19 22:06:33 +0100

    created first draft

--- preload.js
+++ preload.js
@@ -1,8 +1,26 @@
-const { contextBridge } = require('electron')
+/**
+ * The preload script runs before `index.html` is loaded
+ * in the renderer. It has access to web APIs as well as
+ * Electron's renderer process modules and some polyfilled
+ * Node.js functions.
+ *
+ * https://www.electronjs.org/docs/latest/tutorial/sandbox
+ */
+const { contextBridge, ipcRenderer } = require('electron/renderer')
 
-contextBridge.exposeInMainWorld('versions', {
-  node: () => process.versions.node,
-  chrome: () => process.versions.chrome,
-  electron: () => process.versions.electron
-  // we can also expose variables, not just functions
+contextBridge.exposeInMainWorld('electronAPI', {
+  openFolder: () => ipcRenderer.invoke('dialog:openFolder'),
+  loadFolder: (folderPath) => ipcRenderer.invoke('dialog:loadFolder', folderPath)
 })
+
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const type of ['chrome', 'node', 'electron']) {
+    replaceText(`${type}-version`, process.versions[type])
+  }
+})
+
