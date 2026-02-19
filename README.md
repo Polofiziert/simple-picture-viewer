000d3abafc50b5fb4ad23a0eff2a1dd1bf1aaf64
Polofiziert <polo@polofiziert.de>
2026-02-19 21:16:17 +0100

    First demo Electron app

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # simple-picture-viewer
 A Simple Program for looking, sorting and saving at your pictures
+
+FInd a Folder Structure
+
+
+### Installed Dependencies
+- Tailwind CSS ```npx @tailwindcss/cli -i ./src/input.css -o ./src/output.css --watch```is missing....
---
+++ fiddleFiles/jsFiddle/preload.js
@@ -0,0 +1,26 @@
+/**
+ * The preload script runs before `index.html` is loaded
+ * in the renderer. It has access to web APIs as well as
+ * Electron's renderer process modules and some polyfilled
+ * Node.js functions.
+ *
+ * https://www.electronjs.org/docs/latest/tutorial/sandbox
+ */
+const { contextBridge, ipcRenderer } = require('electron/renderer')
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  openFolder: () => ipcRenderer.invoke('dialog:openFolder'),
+  loadFolder: (folderPath) => ipcRenderer.invoke('dialog:loadFolder', folderPath)
+})
+
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const type of ['chrome', 'node', 'electron']) {
+    replaceText(`${type}-version`, process.versions[type])
+  }
+})
+
