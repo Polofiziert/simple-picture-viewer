ee03b4e8912d34234dc9626c58f33201c9343047
Polofiziert <polo@polofiziert.de>
2026-02-23 13:15:42 +0100

    First working implementation of solid-ui with tailwind. linter works just fine. Also pretier make . has made everthing nice looking

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -3,15 +3,15 @@ import { defineConfig } from 'electron-vite'
 import solid from 'vite-plugin-solid'
 
 export default defineConfig({
-  main: {},
-  preload: {},
-  renderer: {
-    resolve: {
-      alias: {
-        '@renderer': resolve('src/renderer/src'),
-        '~': resolve(__dirname, 'src/renderer/src') // From Solid-UI orig: './src'
-      }
-    },
-    plugins: [solid()]
-  }
+    main: {},
+    preload: {},
+    renderer: {
+        resolve: {
+            alias: {
+                '@renderer': resolve('src/renderer/src'),
+                '~': resolve(__dirname, 'src/renderer/src') // From Solid-UI orig: './src'
+            }
+        },
+        plugins: [solid()]
+    }
 })
