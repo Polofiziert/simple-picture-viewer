371a5b1be83ba1e05d1e60bf110b9b8f70ecbdf3
Polofiziert <polo@polofiziert.de>
2026-02-24 00:45:04 +0100

    added doc

--- src/preload/index.d.ts
+++ src/preload/index.d.ts
@@ -2,17 +2,32 @@ import { ElectronAPI } from '@electron-toolkit/preload'
 
 declare global {
     interface WindowControll {
+        /**
+         * minimize() - minimizes the window
+         */
         minimize(): void
+        /**
+         * minimize() - minimizes the window
+         */
         maximize(): void
+        /**
+         * minimize() - minimizes the window
+         */
         close(): void
     }
 
     interface API {
+        /**
+         * contextBridge.api.windowControll - windowControll helper functions
+         */
         windowControll: WindowControll
     }
 
     interface Window {
         electron: ElectronAPI
+        /**
+         * contextBridge.api - mounting point for ipc hanlers of user
+         */
         api: API
     }
 }
